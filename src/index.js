2c14913d2dbe1fdc65dec7a98398b92908882c16
adebayo3035 <adebayoabdulrahmon@gmail.com>
2022-10-31 13:04:49 +0100

    Added Profile Name to the webpage

--- src/index.js
+++ src/index.js
@@ -9,24 +9,33 @@ const Header = () => {
     padding: "10px",
     fontFamily: "Sans-Serif",
     textAlign: "center",
-    // width: "100%",
     textDecoration: "none",
     display: "flex",
     justifyContent: "center",
     margin: "10px"
   };
-  const profile_picture = {
+  const profile_Container = {
     display: "flex",
     justifyContent: "center",
+    flexDirection: "column",
     border: "2px solid red"
+  }
+  const profile_name = {
+    fontWeight: "900",
+    textAlign: "center",
+    fontSize: "26px"
+  }
+  const profile_pic = {
+    width: "100px",
+    alignSelf: "center"
   }
   const myUrl =  {
     twitter: "https://twitter.com/hajji_rahmon",
     zuri_team: "https://training.zuri.team/",
     books: "http://books.zuri.team/",
-    book_python: "https://books.zuri.team/python-for-beginners?ref_id=",
-    pitch: "https://twitter.com/hajji_rahmon",
-    book_design: "https://twitter.com/hajji_rahmon",
+    book_python: "https://books.zuri.team/python-for-beginners?ref_id=abdul-rahmon",
+    pitch: "https://background.zuri.team",
+    book_design: "https://books.zuri.team/design-rules",
   
   };
   const my_id ={
@@ -40,19 +49,22 @@ const Header = () => {
  
   return (
     <>
-      <div style= {profile_picture}>
+      <div style= {profile_Container}>
       <img 
         alt="Your Profie Pic"
-        src ={MyImage} 
+        src ={MyImage}
+        id = "profile_img"
+        style = {profile_pic}
          
       />
+      <p style = {profile_name}> Annette Black </p>
       </div>
       <a style={myStyle} href ={myUrl.twitter} id ={my_id.twitter}>Twitter Link</a>
       <a style={myStyle} href ={myUrl.zuri_team} id ={my_id.btn_zuri}>Zuri Team</a>
       <a style={myStyle} href ={myUrl.books} id ={my_id.books} title="This is where you find Books about Design and Coding">Zuri Books</a>
-      <a style={myStyle} href ={myUrl.book_python} id ={my_id.book_python}>Python Books</a>
-      <a style={myStyle} href ={myUrl.pitch} id ={my_id.pitch}>Background Check for Coders</a>
-      <a style={myStyle} href ={myUrl.book_design} id ={my_id.book_design}>Design Books</a>
+      <a style={myStyle} href ={myUrl.book_python} id ={my_id.book_python} title="Get your Python Books Here">Python Books</a>
+      <a style={myStyle} href ={myUrl.pitch} id ={my_id.pitch} title="Sell your self out here to possible suitors and collaborators">Background Check for Coders</a>
+      <a style={myStyle} href ={myUrl.book_design} id ={my_id.book_design} title="Get free Design Books compiled by Zuri">Design Books</a>
     </>
   );
 }
